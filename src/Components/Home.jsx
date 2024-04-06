32b2bf24f4757c8895598059def0ae2081856a35
SomsubhroChakraborty <somsubhrochakraborty07@gmail.com>
2024-04-06 08:55:31 +0530

    makeing responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,8 +8,9 @@ export default function Home() {
         <h2>Hi 👋,</h2>
         <p> I'm <span className="purple">Somsubhro Chakraborty</span></p> 
         <p><span className="purple">Tech Enthusiast</span> and a passionate<span className="purple"> Developer</span>  from India</p>
+        <h5>Through this platform I invite you to explore a collectction of my <span>Projects</span>.<br/>All of this projects are made when I started the wed development till now</h5>
       </div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMB14UZFhSYB5ijJHWDbSOjiGLyMn6m9gbbw&usqp=CAU" />
+      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMB14UZFhSYB5ijJHWDbSOjiGLyMn6m9gbbw&usqp=CAU" /> */}
     </div>
   );
 }
